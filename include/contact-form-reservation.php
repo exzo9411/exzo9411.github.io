cf7a6752f59640830457c022777f0dca0c490074
exzo94 <w.shraim@gmail.com>
2020-03-13 01:46:26 +0200

    NEW MAIL

--- include/contact-form-reservation.php
+++ include/contact-form-reservation.php
@@ -14,7 +14,7 @@ require 'php-mailer/src/PHPMailer.php';
 $mail = new PHPMailer();
 
 // Enter your email address:
-$to = "exzo@live.com, w.shraim@gmail.com;
+$to = "w.shraim@gmail.com";
 
 // Form Fields
 $name = $_POST["widget-contact-form-name"];
@@ -39,13 +39,13 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST' && isset($antispam) && $antispam == '')
 
                 //If you don't receive the email, enable and configure these parameters below:
 
-                //$mail->isSMTP();                                      // Set mailer to use SMTP
-                //$mail->Host = 'mail.yourserver.com';                  // Specify main and backup SMTP servers, example: smtp1.example.com;smtp2.example.com
-                //$mail->SMTPAuth = true;                               // Enable SMTP authentication
-                //$mail->Username = 'SMTP username';                    // SMTP username
-                //$mail->Password = 'SMTP password';                    // SMTP password
-                //$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-                //$mail->Port = 587;                                    // TCP port to connect to
+                $mail->isSMTP();                                      // Set mailer to use SMTP
+                $mail->Host = 'smtp.gmail.com';                  // Specify main and backup SMTP servers, example: smtp1.example.com;smtp2.example.com
+                $mail->SMTPAuth = true;                               // Enable SMTP authentication
+                $mail->Username = 'w.shraim@gmail.com';                    // SMTP username
+                $mail->Password = 'Plovdiv941..';                    // SMTP password
+                $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+                $mail->Port = 587;                                    // TCP port to connect to                                  // TCP port to connect to
 
      	        $mail->IsHTML(true);                                    // Set email format to HTML
                 $mail->CharSet = 'UTF-8';
--- include/contact-form.php
+++ include/contact-form.php
@@ -14,7 +14,7 @@ require 'php-mailer/src/PHPMailer.php';
 $mail = new PHPMailer();
 
 // Enter your email address. If you need multiple email recipes simply add a comma: email@domain.com, email2@domain.com
-$to = "exzo@live.com, w.shraim@gmail.com";
+$to = "w.shraim@gmail.com";
 
 // Add your reCaptcha Secret key if you wish to activate google reCaptcha security
 $recaptcha_secret_key = '';
@@ -39,13 +39,13 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST') {
 
                 //If you don't receive the email, enable and configure these parameters below:
 
-                //$mail->isSMTP();                                      // Set mailer to use SMTP
-                //$mail->Host = 'mail.yourserver.com';                  // Specify main and backup SMTP servers, example: smtp1.example.com;smtp2.example.com
-                //$mail->SMTPAuth = true;                               // Enable SMTP authentication
-                //$mail->Username = 'SMTP username';                    // SMTP username
-                //$mail->Password = 'SMTP password';                    // SMTP password
-                //$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-                //$mail->Port = 587;                                    // TCP port to connect to
+                $mail->isSMTP();                                      // Set mailer to use SMTP
+                $mail->Host = 'smtp.gmail.com';                  // Specify main and backup SMTP servers, example: smtp1.example.com;smtp2.example.com
+                $mail->SMTPAuth = true;                               // Enable SMTP authentication
+                $mail->Username = 'w.shraim@gmail.com';                    // SMTP username
+                $mail->Password = 'Plovdiv941..';                    // SMTP password
+                $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+                $mail->Port = 587;                                    // TCP port to connect to
 
      	        $mail->IsHTML(true);                                    // Set email format to HTML
                 $mail->CharSet = 'UTF-8';
